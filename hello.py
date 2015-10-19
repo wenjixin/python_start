9f6c1dbf336458ceab2f932c9064a226fd4f4b18
刘桐伟 <tongwei@staff.sina.com.cn>
2015-10-19 23:19:39 +0800

    bak

---
+++ hello.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python
+
+# -*- coding: utf-8 -*-
+
+"""
+hello world.
+
+@author: tongwei
+
+@date: 2015/10/19
+
+"""
+print 'hello world.'
+
+print 'this is a day', 'very happy'
+
+name = raw_input()
+
+print name
